ae1986b9bd0b2c77b8babcf54b2546ddb29e4307
Carlo Pelosi <cpelosi@asu.edu>
2021-01-22 15:37:31 -0700

    add default data for the front pages and some asset images for front

---
+++ src/data/EquipmentData.js
@@ -0,0 +1,23 @@
+import speakers from "../MaterialKitProReact/assets/img/frontpage/speakers.jpg";
+import DJ from "../MaterialKitProReact/assets/img/frontpage/DJ.jpg";
+import catering from "../MaterialKitProReact/assets/img/frontpage/catering.jpg";
+
+const EquipmentData = [
+    {
+        title: "Speakers",
+        description: "We have tons of speakers that can be rented for your next event!",
+        image: speakers
+    },
+    {
+        title: "DJ",
+        description: "Need a local DJ for your community event? We got you covered!",
+        image: DJ
+    },
+    {
+        title: "Catering Services",
+        description: "Want best community service to cater your next event? We have tons of high quality chefs ready to make your next meals",
+        image: catering
+    }
+]
+
+export default EquipmentData;
