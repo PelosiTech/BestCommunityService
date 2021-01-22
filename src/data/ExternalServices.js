d419ce323e423b93936424a94a056aeeb5874520
Carlo Pelosi <cpelosi@asu.edu>
2021-01-22 16:07:27 -0700

    add external services data and asssets images for front page

---
+++ src/data/ExternalServices.js
@@ -0,0 +1,23 @@
+import elderly from "../MaterialKitProReact/assets/img/frontpage/elderly.jpeg";
+import shuttle from "../MaterialKitProReact/assets/img/frontpage/shuttle.jpg";
+import car from "../MaterialKitProReact/assets/img/frontpage/car.jpeg";
+
+const ExternalServices = [
+    {
+        title: "Home Care",
+        description: "Our community is partnered with Health Care Workers to take care of the elderly. We are ready to set you up with at Home Care",
+        image: elderly
+    },
+    {
+        title: "Shuttle Services",
+        description: "Need to get your group to the airport? Or how about just a ride for the night? Check them out today",
+        image: shuttle
+    },
+    {
+        title: "Car Wash Program",
+        description: "Join the rest of the community and sign up for our community monthly car wash service! We come to your house monthly and wash your cars!",
+        image: car
+    }
+]
+
+export default ExternalServices;
