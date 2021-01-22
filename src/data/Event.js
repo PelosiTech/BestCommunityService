1727893419aac766e850208fd7f2b3c7c88832fa
Carlo Pelosi <cpelosi@asu.edu>
2021-01-22 15:38:08 -0700

    add Event data for the front pages and some asset images for front

---
+++ src/data/Event.js
@@ -0,0 +1,23 @@
+import wedding from "../MaterialKitProReact/assets/img/frontpage/wedding-dinner.jpg";
+import meetings from "../MaterialKitProReact/assets/img/frontpage/meetings.jpg";
+import bingo from "../MaterialKitProReact/assets/img/frontpage/bingo.jpg";
+
+const SocialEvents = [
+    {
+        title: "Weddings",
+        description: "Get ready to host your next wedding at our building! We have rooms for rent that can fit up to 250 people!",
+        image: wedding
+    },
+    {
+        title: "Community Meetings",
+        description: "Hosting all of your community meetings at our building! We have rooms with auditoriums and big monitors for your needs!",
+        image: meetings
+    },
+    {
+        title: "Friendly Gatherings",
+        description: "You can invite the entire neighborhood for a game time? Or maybe have your local book club in one of our community rooms!",
+        image: bingo
+    }
+]
+
+export default SocialEvents;
