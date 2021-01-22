94c19b962d7f61c12025b200f78c7022163df710
Carlo Pelosi <cpelosi@asu.edu>
2021-01-22 15:38:25 -0700

    add HouseServices data for front pages and some asset images for front

---
+++ src/data/InHouseServices.js
@@ -0,0 +1,23 @@
+import spanish from "../MaterialKitProReact/assets/img/frontpage/spanish.jpg";
+import arts from "../MaterialKitProReact/assets/img/frontpage/arts.jpg";
+import painting from "../MaterialKitProReact/assets/img/frontpage/painting.jpg";
+
+const InHouseServices = [
+    {
+        title: "Spanish",
+        description: "We have tons of local community Spanish teaches ready to host events in house for your group to learn spanish!",
+        image: spanish
+    },
+    {
+        title: "Arts & Crafts",
+        description: "Need a good idea for a birthday party or want to get your kid to have some fun? We have teachers ready to host Arts & Crafts!",
+        image: arts
+    },
+    {
+        title: "Painting",
+        description: "Ready to take your painting hobby to the next level? We have incredible teachers hosting tons of events taking new levels of the knowledge of painting.",
+        image: painting
+    }
+]
+
+export default InHouseServices;
