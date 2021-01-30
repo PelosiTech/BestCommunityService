49adca4c8a7c219b2559658a3c333925be2c9683
Carlo Pelosi <cpelosi@asu.edu>
2021-01-29 22:24:06 -0700

    final touches to confirmation page

--- src/pages/ConfirmationPage.js
+++ src/pages/ConfirmationPage.js
@@ -12,20 +12,12 @@ import HeaderLinks from "../MaterialKitProReact/components/Header/HeaderLinks.js
 import Parallax from "../MaterialKitProReact/components/Parallax/Parallax.js";
 import GridContainer from "../MaterialKitProReact/components/Grid/GridContainer.js";
 import GridItem from "../MaterialKitProReact/components/Grid/GridItem.js";
-import Button from "../MaterialKitProReact/components/CustomButtons/Button.js";
-import Accordion from "../MaterialKitProReact/components/Accordion/Accordion.js";
 
 import productStyle from "../MaterialKitProReact/assets/jss/material-kit-pro-react/views/productStyle.js";
 import {API, graphqlOperation} from "aws-amplify";
-import {getBooked, getService} from "../graphql/queries";
+import {getBooked} from "../graphql/queries";
 import Card from "../MaterialKitProReact/components/Card/Card";
 import CardHeader from "../MaterialKitProReact/components/Card/CardHeader";
-import Dialog from "@material-ui/core/Dialog";
-import DialogTitle from "@material-ui/core/DialogTitle";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogActions from "@material-ui/core/DialogActions";
-import {Close} from "@material-ui/icons";
-import Transition from "react-transition-group/Transition";
 import style from "../MaterialKitProReact/assets/jss/material-kit-pro-react/modalStyle.js";
 import {createBooked} from "../graphql/mutations";
 import {useSelector} from "react-redux";
@@ -44,9 +36,7 @@ export default function ConfirmationPage(props) {
     const [bookingUser, setBookingUser] = React.useState({});
     const [data, setData] = React.useState({});
     const [bookedUsers, setBookedUsers] = React.useState([]);
-    const [showModal, setShowModal] = React.useState(false);
     const classes = useStyles();
-    const modalClasses = useModalStyles();
     const id = props.location.pathname.slice(14);
     const userId = useSelector((state) => state.auth.id);
     const history = useHistory();
@@ -106,97 +96,10 @@ export default function ConfirmationPage(props) {
                     </Card>
                 </GridItem>
                 <GridItem md={6} sm={6}>
-                    <h2 className={classes.title}>{data.name}</h2>
-                    <h3 className={classes.mainPrice}>${data.cost}</h3>
-                    <Accordion
-                        active={0}
-                        activeColor="info"
-                        collapses={[
-                            {
-                                title: "Description",
-                                content: (
-                                    <p>
-                                        {data.description}
-                                    </p>
-                                )
-                            },
-                            {
-                                title: "Date and Availability",
-                                content: (
-                                    <>
-                                        <div>Date: {data.date}</div>
-                                        <div>Spots open: {data.quantity}</div>
-                                        <div>Type: {data.type}</div>
-                                    </>
-                                )
-                            },
-                            {
-                                title: "Details",
-                                content: (
-                                    <ul>
-                                        <li>Created By: {data.user.name}</li>
-                                        <li>
-                                            Current Users who have this booked:
-                                            {renderBookedUsersList()}
-                                        </li>
-                                    </ul>
-                                )
-                            }
-                        ]}
-                    />
-                    <GridContainer className={classes.pullRight}>
-                        <div>
-                            <Button color="info" onClick={() => setShowModal(true)}>
-                                Book Now
-                            </Button>
-                            <Dialog
-                                classes={{
-                                    root: modalClasses.modalRoot,
-                                    paper: modalClasses.modal
-                                }}
-                                open={showModal}
-                                TransitionComponent={Transition}
-                                keepMounted
-                                onClose={() => setShowModal(false)}
-                                aria-labelledby="classic-modal-slide-title"
-                                aria-describedby="classic-modal-slide-description"
-                            >
-                                <DialogTitle
-                                    id="classic-modal-slide-title"
-                                    disableTypography
-                                    className={modalClasses.modalHeader}
-                                >
-                                    <Button
-                                        simple
-                                        className={modalClasses.modalCloseButton}
-                                        key="close"
-                                        aria-label="Close"
-                                        onClick={() => setShowModal(false)}
-                                    >
-                                        {" "}
-                                        <Close className={modalClasses.modalClose} />
-                                    </Button>
-                                    <h2 className={modalClasses.modalTitle}>Event Details</h2>
-                                </DialogTitle>
-                                <DialogContent
-                                    id="classic-modal-slide-description"
-                                    className={modalClasses.modalBody}
-                                >
-                                    <div>
-                                        <h4 className={classes.title}>Event name: {data.name}</h4>
-                                        <h4 className={classes.mainPrice}>Event cost: ${data.cost}</h4>
-                                        <h4 className={classes.mainPrice}>Event Date: {data.date}</h4>
-                                    </div>
-                                </DialogContent>
-                                <DialogActions className={modalClasses.modalFooter}>
-                                    <Button onClick={() => setShowModal(false)} color="secondary">
-                                        Close
-                                    </Button>
-                                    <Button onClick={() => handleBookNow()} color="info">Book NOW</Button>
-                                </DialogActions>
-                            </Dialog>
-                        </div>
-                    </GridContainer>
+                    <h3 className={classes.title}> Congratulations {bookingUser.name}</h3>
+                    <h3 className={classes.title}> You are all booked in for {data.name}</h3>
+                    <h4 className={classes.title}> Your booking date is {data.date}</h4>
+                    <h3 className={classes.title}> You confirmation code is {bookingUser.id}</h3>
                 </GridItem>
             </GridContainer>
         )
