3ccb457ef95267bfb1da2e1e492a0ed8139b96ef
Carlo Pelosi <cpelosi@asu.edu>
2021-02-10 22:33:32 -0700

    update donations page for taking donations, mutations, and redux saving

--- src/pages/DonationsPage.js
+++ src/pages/DonationsPage.js
@@ -19,7 +19,7 @@ import Datetime from "react-datetime";
 
 import productStyle from "../MaterialKitProReact/assets/jss/material-kit-pro-react/views/productStyle.js";
 import {Storage, API, graphqlOperation} from "aws-amplify";
-import {getService} from "../graphql/queries";
+import {getService, listUsers} from "../graphql/queries";
 import Card from "../MaterialKitProReact/components/Card/Card";
 import CardHeader from "../MaterialKitProReact/components/Card/CardHeader";
 import Dialog from "@material-ui/core/Dialog";
@@ -30,7 +30,7 @@ import {Close} from "@material-ui/icons";
 import Transition from "react-transition-group/Transition";
 import style from "../MaterialKitProReact/assets/jss/material-kit-pro-react/modalStyle.js";
 import {createBooked, createService, updateUser} from "../graphql/mutations";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import awsExports from '../aws-exports';
 import CustomInput from "../MaterialKitProReact/components/CustomInput/CustomInput";
 import Select from "@material-ui/core/Select";
@@ -38,6 +38,8 @@ import MenuItem from "@material-ui/core/MenuItem";
 import InputLabel from "@material-ui/core/InputLabel";
 import FormControl from "@material-ui/core/FormControl";
 import moment from "moment";
+import {login} from "../Redux/actions/authActions";
+import {listUsersDonations} from "../queries/CustomQueries";
 
 // images
 
@@ -50,27 +52,53 @@ export default function DonationsPage(props) {
         document.body.scrollTop = 0;
     });
     const history = useHistory();
+    const dispatch = useDispatch();
     const [donationAmount, setDonationAmount] = React.useState('');
     const [showModal, setShowModal] = React.useState(false)
+    const [showThankYou, setShowThankYou] = React.useState(false)
+    const [donatedUsers, setDonatedUsers] = React.useState([])
     const classes = useStyles();
     const modalClasses = useModalStyles();
     const userId = useSelector((state) => state.auth.id);
+    const currentDonate = useSelector((state) => state.auth.donationAmount);
 
     const handleCreateEvent = async () => {
-        // try {
-        //     await API.graphql(graphqlOperation(updateUser,{
-        //         input: {
-        //             userId,
-        //             donationAmount
-        //         }
-        //     })).then(data => {
-        //         console.log(data)
-        //         history.push(`/confirmation/${info.data.updateUser.id}`)
-        //     })
-        // } catch (err) {
-        //     console.log(err)
-        // }
+        let amount;
+        if (currentDonate === null) {
+            amount = parseInt(donationAmount)
+        } else {
+            amount = parseInt(donationAmount) + parseInt(currentDonate)
+        }
+        try {
+            await API.graphql(graphqlOperation(updateUser,{
+                input: {
+                    id: userId,
+                    donationAmount: parseInt(amount)
+                }
+            })).then(data => {
+                console.log(data)
+                const graphQLUser = data.data.updateUser;
+                dispatch(login(graphQLUser.id, graphQLUser.email, graphQLUser.name, graphQLUser.position, graphQLUser.services, graphQLUser.donationAmount));
+                setShowThankYou(true)
+            })
+        } catch (err) {
+            console.log(err)
+        }
     }
+
+    useEffect(() => {
+        try {
+            const getUsersDonations = async () => {
+                await API.graphql(graphqlOperation(listUsersDonations)).then(data => {
+                    console.log(data.data.listUsers)
+
+                })
+            }
+            getUsersDonations();
+        } catch (err) {
+            console.log(err)
+        }
+    }, [])
 
     const renderPage = () => {
 
@@ -88,71 +116,84 @@ export default function DonationsPage(props) {
                         </CardHeader>
                     </Card>
                 </GridItem>
-                <GridItem md={6} sm={6}>
-                    <div>Thank you so much for your donation! We really appreciate it!</div>
-                    <h2 className={classes.title}>Enter in your donation amount: </h2>
-                    <h3 className={classes.title}>Amount: </h3>
-                    <CustomInput
-                        labelText="Amount"
-                        id="amount"
-                        formControlProps={{
-                            fullWidth: true
-                        }}
-                        inputProps={{onChange: (e)=> setDonationAmount(e.target.value)}}
-                    />
-                    <GridContainer className={classes.pullRight}>
-                        <div>
-                            <Button color="info" onClick={() => setShowModal(true)}>
-                                Donate now
-                            </Button>
-                            <Dialog
-                                classes={{
-                                    root: modalClasses.modalRoot,
-                                    paper: modalClasses.modal
-                                }}
-                                open={showModal}
-                                TransitionComponent={Transition}
-                                keepMounted
-                                onClose={() => setShowModal(false)}
-                                aria-labelledby="classic-modal-slide-title"
-                                aria-describedby="classic-modal-slide-description"
-                            >
-                                <DialogTitle
-                                    id="classic-modal-slide-title"
-                                    disableTypography
-                                    className={modalClasses.modalHeader}
+                { showThankYou
+                    ?
+                    <GridItem md={6} sm={6}>
+                        <Card product plain>
+                            <CardHeader image plain>
+                                <h2 className={classes.title}>We have received your donation!</h2>
+                                <h2 className={classes.title}>${donationAmount}!</h2>
+                                <h2 className={classes.title}>Very generous of you!</h2>
+                            </CardHeader>
+                        </Card>
+                    </GridItem>
+                    :
+                    <GridItem md={6} sm={6}>
+                        <div>Thank you so much for your donation! We really appreciate it!</div>
+                        <h2 className={classes.title}>Enter in your donation amount: </h2>
+                        <h3 className={classes.title}>Amount: </h3>
+                        <CustomInput
+                            labelText="Amount"
+                            id="amount"
+                            formControlProps={{
+                                fullWidth: true
+                            }}
+                            inputProps={{onChange: (e)=> setDonationAmount(e.target.value)}}
+                        />
+                        <GridContainer className={classes.pullRight}>
+                            <div>
+                                <Button color="info" onClick={() => setShowModal(true)}>
+                                    Donate now
+                                </Button>
+                                <Dialog
+                                    classes={{
+                                        root: modalClasses.modalRoot,
+                                        paper: modalClasses.modal
+                                    }}
+                                    open={showModal}
+                                    TransitionComponent={Transition}
+                                    keepMounted
+                                    onClose={() => setShowModal(false)}
+                                    aria-labelledby="classic-modal-slide-title"
+                                    aria-describedby="classic-modal-slide-description"
                                 >
-                                    <Button
-                                        simple
-                                        className={modalClasses.modalCloseButton}
-                                        key="close"
-                                        aria-label="Close"
-                                        onClick={() => setShowModal(false)}
+                                    <DialogTitle
+                                        id="classic-modal-slide-title"
+                                        disableTypography
+                                        className={modalClasses.modalHeader}
                                     >
-                                        {" "}
-                                        <Close className={modalClasses.modalClose} />
-                                    </Button>
-                                    <h2 className={modalClasses.modalTitle}>We really appreciate it!</h2>
-                                </DialogTitle>
-                                <DialogContent
-                                    id="classic-modal-slide-description"
-                                    className={modalClasses.modalBody}
-                                >
-                                    <div>
-                                        <h3>Thank you so much for your donation!</h3>
-                                        <h4 className={classes.title}>Donation Amount: ${donationAmount}</h4>
-                                    </div>
-                                </DialogContent>
-                                <DialogActions className={modalClasses.modalFooter}>
-                                    <Button onClick={() => setShowModal(false)} color="secondary">
-                                        Cancel
-                                    </Button>
-                                    <Button onClick={() => handleCreateEvent()} color="info">Donate!</Button>
-                                </DialogActions>
-                            </Dialog>
-                        </div>
-                    </GridContainer>
-                </GridItem>
+                                        <Button
+                                            simple
+                                            className={modalClasses.modalCloseButton}
+                                            key="close"
+                                            aria-label="Close"
+                                            onClick={() => setShowModal(false)}
+                                        >
+                                            {" "}
+                                            <Close className={modalClasses.modalClose} />
+                                        </Button>
+                                        <h2 className={modalClasses.modalTitle}>We really appreciate it!</h2>
+                                    </DialogTitle>
+                                    <DialogContent
+                                        id="classic-modal-slide-description"
+                                        className={modalClasses.modalBody}
+                                    >
+                                        <div>
+                                            <h3>Thank you so much for your donation!</h3>
+                                            <h4 className={classes.title}>Donation Amount: ${donationAmount}</h4>
+                                        </div>
+                                    </DialogContent>
+                                    <DialogActions className={modalClasses.modalFooter}>
+                                        <Button onClick={() => setShowModal(false)} color="secondary">
+                                            Cancel
+                                        </Button>
+                                        <Button onClick={() => handleCreateEvent()} color="info">Donate!</Button>
+                                    </DialogActions>
+                                </Dialog>
+                            </div>
+                        </GridContainer>
+                    </GridItem>
+                }
             </GridContainer>
         )
     }
