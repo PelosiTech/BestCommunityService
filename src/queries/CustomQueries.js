3972f5923e8311c32ade0dc8a993dd1c0560f52c
Carlo Pelosi <cpelosi@asu.edu>
2021-02-10 22:33:08 -0700

    build custom query for getting donations and names

---
+++ src/queries/CustomQueries.js
@@ -0,0 +1,16 @@
+export const listUsersDonations = /* GraphQL */ `
+  query ListUsers(
+    $filter: ModelUserFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listUsers(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        name
+        donationAmount
+      }
+      nextToken
+    }
+  }
+`;
